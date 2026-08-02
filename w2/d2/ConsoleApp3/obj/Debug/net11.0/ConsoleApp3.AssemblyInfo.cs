314d480f2f808057d0886ddcabe7c1e4444a865f
leenazzam <leenazzamsh@gmail.com>
2026-08-02 23:25:55 +0300

    w3d1

--- w2/d2/ConsoleApp3/obj/Debug/net11.0/ConsoleApp3.AssemblyInfo.cs
+++ w2/d2/ConsoleApp3/obj/Debug/net11.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+979631638bb9681c413e13cb9ffcc7b77a9faf4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f70e7183c1ed509943f5846a1f991154a4cb5d64")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
