fe8344eb931c621cadc812671e319e9ebbb8a6b1
leenazzam <leenazzamsh@gmail.com>
2026-08-04 21:55:21 +0300

    w3d3

---
+++ w3/d3/README.md
@@ -0,0 +1,4 @@
+# Day 3: EF Core Setup
+
+This project demonstrates how to integrate Entity Framework Core with an ASP.NET Core Web API. It includes entity models, a DbContext, SQL Server configuration, and the initial database migration. The database was created using EF Core migrations based on the ERD designed in Week 3 Day 2.
+![alt text](image.png)
---
+++ w3/d3/WebApplication1/Controllers/TasksController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppTask>>> GetTasks()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+
+        // GET: api/tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppTask>> GetTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null)
+                return NotFound();
+
+            return task;
+        }
+
+
+        // POST: api/tasks
+        [HttpPost]
+        public async Task<ActionResult<AppTask>> CreateTask(AppTask task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetTask),
+                new { id = task.Id },
+                task
+            );
+        }
+    }
+}
